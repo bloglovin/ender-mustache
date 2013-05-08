3690548f5bb91dc3ec8b8f2ddecec9975014db54
David Byrd <david@byrdhou.se>
2013-05-08 17:50:18 -0400

    init

---
+++ src/copyright.js
@@ -0,0 +1,8 @@
+/*!
+ * mustache.js - Logic-less {{mustache}} templates with JavaScript
+ * http://github.com/janl/mustache.js
+ *
+ * The MIT License
+ * Copyright (c) 2009 Chris Wanstrath (Ruby)
+ * Copyright (c) 2010 Jan Lehnardt (JavaScript)
+*/
---
+++ src/ender.js
@@ -0,0 +1,5 @@
+var mustache = require('mustache')
+
+ender.ender({
+  render: mustache.render
+})
